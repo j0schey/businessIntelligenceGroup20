12b0d03562f126207dedf55dbd1d3a853f874e69
mbr501 <michael.g.bauer@gmail.com>
2017-12-05 18:30:12 +0100

    Delete TB_ProductCategory.sql

--- 01_HandIn/task1/TB_ProductCategory.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE TB_ProductCategory(
-	ProductCategoryID int NOT NULL,
-    ParentProductCategoryID int,
-    Name varchar(255) NOT NULL,
-    CONSTRAINT PK_ProductCategory PRIMARY KEY (ProductCategoryID),
-    CONSTRAINT FK_ParentProductCategory_ProductCategory FOREIGN KEY (ParentProductCategoryID)
-    REFERENCES TB_ProductCategory(ProductCategoryID)
-)
